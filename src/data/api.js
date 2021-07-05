c33166cbeae9246c90239a6d672606171184921f
yassine299 <yassinehajjoubi299@gmail.com>
2021-07-05 21:01:34 +0100

    editing pics link

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const apiUrl = "http://localhost:8080/api/";
+const apiUrl = "https://uploadfilenodejs.herokuapp.com/";
 
 export const singleFileUpload = async (data) => {
     try {
